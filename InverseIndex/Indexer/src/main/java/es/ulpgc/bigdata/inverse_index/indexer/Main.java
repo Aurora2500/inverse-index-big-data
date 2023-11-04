3915d415c5e6ae437fa1ea7a5fb93f5a6736a618
Aurora <programming.aurora@gmail.com>
2023-11-04 23:52:18 +0000

    Implemented Indexer

---
+++ InverseIndex/Indexer/src/main/java/es/ulpgc/bigdata/inverse_index/indexer/Main.java
@@ -0,0 +1,24 @@
+package es.ulpgc.bigdata.inverse_index.indexer;
+
+import es.ulpgc.bigdata.inverse_index.datamart.Datamart;
+import es.ulpgc.bigdata.inverse_index.datamart.file.FileDatamart;
+
+import java.nio.file.Path;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		//assert that the arguments are correct
+		if (args.length != 2) {
+			System.out.println("Usage: java -jar indexer.jar <datalake> <datamart>");
+			System.out.println("Num args: " + args.length);
+			for(String arg: args) {
+				System.out.println(arg);
+			}
+			System.exit(1);
+		}
+		FileDatalake datalake = new FileDatalake(Path.of(args[0]));
+		Datamart datamart = new FileDatamart(Path.of(args[1]));
+		Indexer indexer = new Indexer(datalake, datamart);
+		indexer.index();
+	}
+}
