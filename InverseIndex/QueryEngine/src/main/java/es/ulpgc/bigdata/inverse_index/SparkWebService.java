f6d482dba3cdcd1cd4ee8ea94144aba0f88022d3
Alejandra-RAF <alejandraruizdeadanafleitas9@gmail.com>
2023-11-05 14:49:04 +0000

    crawler

--- InverseIndex/QueryEngine/src/main/java/es/ulpgc/bigdata/inverse_index/SparkWebService.java
+++ InverseIndex/QueryEngine/src/main/java/es/ulpgc/bigdata/inverse_index/SparkWebService.java
@@ -14,39 +14,17 @@ public class SparkWebService {
         Spark.port(4567);
 
         Spark.get("/v1/search", (request, response) -> {
-            String id = request.queryParams("id");
             String word = request.queryParams("word");
 
-            if (id != null) {
-                // Procesa la búsqueda por ID
-                try {
-                    int documentId = Integer.parseInt(id);
-                    Document document = SqliteReader.retrieveDocumentById(documentId);
-
-                    if (document != null) {
-                        Gson gson = new Gson();
-                        String json = gson.toJson(document);
-                        response.type("application/json");
-                        return json;
-                    } else {
-                        response.status(404); // Not Found
-                        return "Document not found for the specified ID.";
-                    }
-                } catch (NumberFormatException e) {
-                    response.status(400); // Bad Request
-                    return "Invalid ID. It must be an integer.";
-                }
-            } else if (word != null) {
-                // Procesa la búsqueda por palabra
+            if (word != null) {
                 try {
                     SqliteReader sqliteReader = new SqliteReader();
                     List<String> books = sqliteReader.getBooksForWord(word);
 
                     if (!books.isEmpty()) {
-                        List<Document> documents = sqliteReader.getMetadataForBooks(books);
                         Gson gson = new Gson();
                         response.type("application/json");
-                        return gson.toJson(documents);
+                        return gson.toJson(books);
                     } else {
                         response.status(404); // Not Found
                         return "Documents not found for the specified word.";
@@ -56,7 +34,6 @@ public class SparkWebService {
                     return "Error processing the request: " + e.getMessage();
                 }
             } else {
-                // Manejar caso en el que no se proporciona ni "id" ni "word"
                 response.status(400); // Bad Request
                 return "You must provide either 'id' or 'word' as a query parameter.";
             }
--- InverseIndex/QueryEngine/src/main/java/es/ulpgc/bigdata/inverse_index/SqliteReader.java
+++ InverseIndex/QueryEngine/src/main/java/es/ulpgc/bigdata/inverse_index/SqliteReader.java
@@ -5,107 +5,27 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 
 public class SqliteReader {
 
-
-    public static Document retrieveDocumentById(int id) {
-        Document document = null;
-        String url = "jdbc:sqlite:metadata.db";
-
-        try (Connection connection = DriverManager.getConnection(url);
-             PreparedStatement preparedStatement = connection.prepareStatement("SELECT id, date, author, text, title, lang FROM metadata WHERE id = ?")) {
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                int documentId = resultSet.getInt("id");
-                String date = resultSet.getString("date");
-                String author = resultSet.getString("author");
-                String text = resultSet.getString("text");
-                String title = resultSet.getString("title");
-                String lang = resultSet.getString("lang");
-
-                document = new Document(documentId, date, author, text, title, lang);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return document;
-    }
-
     public List<String> getBooksForWord(String word) {
         List<String> books = new ArrayList<>();
-        String url = "jdbc:sqlite:metadata.db";
+
+        String url = "jdbc:sqlite:datamart.db";
 
         try (Connection connection = DriverManager.getConnection(url);
-             PreparedStatement preparedStatement = connection.prepareStatement("SELECT word, book FROM words WHERE word = ?")) {
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT book FROM words WHERE words = ?")) {
             preparedStatement.setString(1, word);
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            if (resultSet.next()) {
-                String booksStr = resultSet.getString("book");
-                books = Arrays.asList(booksStr.split(", "));
+            while (resultSet.next()) {
+                String book = resultSet.getString("book");
+                books.add(book);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return books;
     }
-
-    public List<Document> getMetadataForBooks(List<String> books) {
-        ArrayList<Document> documents = new ArrayList<>();
-        String url = "jdbc:sqlite:metadata.db";
-
-        try (Connection connection = DriverManager.getConnection(url)) {
-            for (String book : books) {
-                try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT id, title, text, lang, date, author FROM metadata WHERE title = ?")) {
-                    preparedStatement.setString(1, book);
-                    ResultSet resultSet = preparedStatement.executeQuery();
-
-                    if (resultSet.next()) {
-                        int documentId = resultSet.getInt("id");
-                        String date = resultSet.getString("date");
-                        String author = resultSet.getString("author");
-                        String text = resultSet.getString("text");
-                        String title = resultSet.getString("title");
-                        String lang = resultSet.getString("lang");
-
-                        StringBuilder first200Words = null;
-                        if (text != null) {
-                            // Split the text into words
-                            String[] words = text.split("\\s+");
-
-                            // Take the first 200 words or less if the text has less than 200 words
-                            first200Words = new StringBuilder();
-                            for (int i = 0; i < Math.min(words.length, 200); i++) {
-                                first200Words.append(words[i]).append(" ");
-                            }
-                            // Append "..." to the end if there are more words in the original text
-                            if (words.length > 200) {
-                                first200Words.append("...");
-                            }
-                        }
-                        Document document = new Document(documentId, date, author, first200Words.toString(), title, lang);
-                        documents.add(document);
-
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return documents;
-    }
-
 }
-
-
-
-
-
