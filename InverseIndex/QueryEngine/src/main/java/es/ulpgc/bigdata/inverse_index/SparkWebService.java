415640cb630bce8a0b865b69a0e8ab8e88d25ca1
Aris64 <arissoriano71@gmail.com>
2023-12-14 23:32:10 +0000

    header of server info in response

--- InverseIndex/QueryEngine/src/main/java/es/ulpgc/bigdata/inverse_index/SparkWebService.java
+++ InverseIndex/QueryEngine/src/main/java/es/ulpgc/bigdata/inverse_index/SparkWebService.java
@@ -1,8 +1,5 @@
 package es.ulpgc.bigdata.inverse_index;
 
-
-
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -25,15 +22,16 @@ public class SparkWebService {
     }
 
     public void startServer() {
-        Spark.port(4567);
+        Spark.port(8080);
 
         Spark.get("/v1/search", (request, response) -> {
             String word = request.queryParams("word");
+            String backendServer = request.headers("X-Backend-Server");
             if (word == null) {
                 response.status(400);
                 return "Missing word parameter\n";
             }
-            // pagination
+
             String offsetStr = request.queryParams("offset");
             String limitStr = request.queryParams("limit");
             int offset = offsetStr == null ? 0 : Integer.parseInt(offsetStr);
@@ -41,9 +39,14 @@ public class SparkWebService {
             limit = Math.min(limit, MAX_LIMIT);
             try {
                 Set<Integer> documentIds = datamart.index(word);
-                // get a sorted list of document ids
+
                 List<Integer> sortedIds = documentIds.stream().sorted().toList();
+                response.header("X-Backend-Server", "ServidorSpark");
                 response.type("application/json");
+
+                StringBuilder serverMsg = new StringBuilder("Connected to server: " + backendServer + "\n\n");
+                System.out.println(serverMsg);
+
                 JsonArray documents = new JsonArray();
                 for (int i = offset; i < Math.min(offset + limit, sortedIds.size()); i++) {
                     int id = sortedIds.get(i);
@@ -51,7 +54,7 @@ public class SparkWebService {
                     JsonObject el = summaryJsonObject(id, document);
                     documents.add(el);
                 }
-                return gson.toJson(documents) + "\n";
+                return serverMsg.toString() + gson.toJson(documents) + "\n";
             } catch (Exception e) {
                 response.status(500);
                 return "Internal server error\n";
@@ -60,6 +63,7 @@ public class SparkWebService {
 
         Spark.get("/v1/document/:id", (request, response) -> {
             String idStr = request.params(":id");
+            String backendServer = request.headers("X-Backend-Server");
             if (idStr == null) {
                 response.status(400);
                 return "Missing id parameter\n";
@@ -67,8 +71,13 @@ public class SparkWebService {
             try {
                 int id = Integer.parseInt(idStr);
                 Document document = datalake.getDocument(id);
+                response.header("X-Backend-Server", "ServidorSpark");
                 response.type("application/json");
-                return gson.toJson(document);
+
+                String serverMsg = "Connected to server: " + backendServer + "\n\n";
+                System.out.println(serverMsg);
+                return serverMsg + gson.toJson(document);
+
             } catch (Exception e) {
                 response.status(500);
                 return "Internal server error\n";
@@ -80,20 +89,20 @@ public class SparkWebService {
         JsonObject el = new JsonObject();
         el.addProperty("id", id);
         if (document.title != null) {
-	        el.addProperty("title", document.title);
+            el.addProperty("title", document.title);
         }
         if (document.author != null) {
-          el.addProperty("author", document.author);
+            el.addProperty("author", document.author);
         }
         if (document.date != null) {
-          el.addProperty("date", document.date);
+            el.addProperty("date", document.date);
         }
         if (document.language != null) {
-          el.addProperty("language", document.language);
+            el.addProperty("language", document.language);
         }
         String summary = document.content.length() > SUMMARY_LENGTH ?
-            document.content.substring(0, SUMMARY_LENGTH) + "..." :
-            document.content;
+                document.content.substring(0, SUMMARY_LENGTH) + "..." :
+                document.content;
         el.addProperty("summary", summary);
         el.addProperty("length", document.content.length());
         return el;
